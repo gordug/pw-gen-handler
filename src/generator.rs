01715d5d918a5ca915fbb604f74ee2e0221c25e8
Chris Johnson <chris.johnson@traka.com>
2023-05-30 20:19:23 +0100

    Added required character options Migrated Options to options.rs Migrated Generator to generator.rs

---
+++ src/generator.rs
@@ -0,0 +1,46 @@
+use crate::options::{self, PasswordOptions, PasswordTypes};
+use pw_gen::password::{PasswordGenerator, generator::Generator};
+use tracing::{info};
+
+#[tracing::instrument]
+pub(crate) fn generate_password (option: Option<PasswordOptions>) -> String {
+    let options = option.unwrap_or(PasswordOptions::default());
+    let length = options.length;
+    info!("Generating password with length {} and options {:?}", length, options);
+    let mut generator = &mut Generator::new(length as usize);
+
+    for password_type in options.password_type.unwrap_or(options::default_password_type()) {
+        match password_type {
+            PasswordTypes::Special { required } => {
+                generator = generator.with_special(required);
+            }
+            PasswordTypes::Numbers { required } => {
+                generator = generator.with_numbers(required);
+            }
+            PasswordTypes::Lowercase { required } => {
+                generator = generator.with_lowercase(required);
+            }
+            PasswordTypes::Uppercase { required } => {
+                generator = generator.with_uppercase(required);
+            }
+        }
+    }
+
+    if options.no_ambiguous.unwrap_or(false) {
+        generator = generator.without_ambiguous();
+    }
+
+    if options.no_similar.unwrap_or(false) {
+        generator = generator.without_similar();
+    }
+
+    if options.no_sequential.unwrap_or(false) {
+        generator = generator.without_sequential();
+    }
+
+    let password = generator.generate();
+
+
+    info!("Generated password: {}", password);
+    password
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+mod generator;
 mod options;
 
+
 use std::{env, net::Ipv4Addr};
-use tracing::{info};
 use warp::{http::Response, Filter};
-use options::{PasswordOptions, PasswordTypes} ;
-use pw_gen::password::{PasswordGenerator, generator::Generator};
 
 #[tracing::instrument]
 #[tokio::main]
@@ -21,7 +20,7 @@ async fn main() {
         .and(warp::path("GeneratePassword"))
         .and(warp::body::json())
         .map(|options| {
-            let password = crate::generate_password(options);
+            let password = generator::generate_password(options);
             Response::builder()
                 .header("Content-Type", "text/plain")
                 .body(password)
@@ -29,49 +28,4 @@ async fn main() {
         .with(warp::trace::request());
 
     warp::serve(routes).run((Ipv4Addr::UNSPECIFIED, port)).await;
-}
-
-
-
-#[tracing::instrument]
-fn generate_password (option: Option<PasswordOptions>) -> String {
-    let options = option.unwrap_or(PasswordOptions::default());
-    let length = options.length;
-    info!("Generating password with length {} and options {:?}", length, options);
-    let mut generator = &mut Generator::new(length as usize);
-
-    for password_type in options.password_type.unwrap_or(options::default_password_type()) {
-        match password_type {
-            PasswordTypes::Special { required } => {
-                generator = generator.with_special(required);
-            }
-            PasswordTypes::Numbers { required } => {
-                generator = generator.with_numbers(required);
-            }
-            PasswordTypes::Lowercase { required } => {
-                generator = generator.with_lowercase(required);
-            }
-            PasswordTypes::Uppercase { required } => {
-                generator = generator.with_uppercase(required);
-            }
-        }
-    }
-
-    if options.no_ambiguous.unwrap_or(false) {
-        generator = generator.without_ambiguous();
-    }
-
-    if options.no_similar.unwrap_or(false) {
-        generator = generator.without_similar();
-    }
-
-    if options.no_sequential.unwrap_or(false) {
-        generator = generator.without_sequential();
-    }
-
-    let password = generator.generate();
-
-
-    info!("Generated password: {}", password);
-    password
 }
